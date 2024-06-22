c744354f664f2cfa9394342809b9e5da3369d9d3
sairaj chikyala <sairaj.chikyala@gmail.com>
2024-06-22 11:23:11 +0530

    create all pipelines

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
 component=$1
- ansible-playbook -i component-dev.sairajd.online, -e ansible-user=ec2-user -e ansible-pass=DevOps321 -e env=dev -e role_name=$component expense.yml
+ ansible-playbook -i $component-$env.sairajd.online, -e ansible-user=ec2-user -e ansible-pass=DevOps321 -e env=$env -e role_name=$component expense.yml
